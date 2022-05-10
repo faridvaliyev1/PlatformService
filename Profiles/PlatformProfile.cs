c9d8b7bdc9fc2f3677a9b5eeb3d5755ca5f2af74
Farid Valiyev <farid@Farids-MacBook-Air.local>
2022-05-11 00:24:48 +0300

    Adding profiles

---
+++ Profiles/PlatformProfile.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Profiles{
+    public class PlatformProfile:Profile{
+        public PlatformProfile()
+        {
+            // source to target
+            CreateMap<Platform,PlatformReadDto>();
+            CreateMap<PlatformCreateDto,Platform>();
+        }
+    }
+}
